94212f6fdc2b9f135f69e42392485adce8b04cc7
Pierre Monnin <pier.monn@gmail.com>
2014-05-19 10:53:10 +0200

    Indent better than Videl :)

--- util/metadata.h
+++ util/metadata.h
@@ -6,27 +6,27 @@
 
 typedef struct Metadata
 {
-  /**
-   * File's name.
-   */
-  char md_name[255];
+    /**
+    * File's name.
+    */
+    char md_name[255];
 
-  /**
-   * File's extension.
-   */
-  char md_extension[10];
+    /**
+    * File's extension.
+    */
+    char md_extension[10];
 
-  /**
-   * List of keywords to describe the file.
-   */
-  char md_keywords[255];
+    /**
+    * List of keywords to describe the file.
+    */
+    char md_keywords[255];
 
-  /**
-   * Hash of the content. We use SHA-1 which generates a
-   * 160-bit hash value rendered as 40 digits long in
-   * hexadecimal.
-   */
-  char md_hash[41];
+    /**
+    * Hash of the content. We use SHA-1 which generates a
+    * 160-bit hash value rendered as 40 digits long in
+    * hexadecimal.
+    */
+    char md_hash[41];
 } Metadata;
 
 Metadata *new_metadata(char name[], char extension[], char keywords[], char hash[]);
