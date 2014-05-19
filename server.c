06d831d84bdeb44658e3ecfb67fedfe1d421e922
Pierre Monnin <pier.monn@gmail.com>
2014-05-19 16:25:16 +0200

    Fix

--- server.c
+++ server.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <sting.h>
+#include <string.h>
+#include <netinet/in.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include "util/metadata.h"
@@ -13,7 +14,6 @@ int main(int argc, char *argv[])
 	struct sockaddr_in serv_addr;
 	struct sockaddr_in client_addr;
 	int clilen = sizeof(client_addr);
-
 
 	/**
 	* First open the listening socket
@@ -32,12 +32,11 @@ int main(int argc, char *argv[])
 	serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
 	serv_addr.sin_port = htons(PORT_ON_SERV);
 
-	if(bind(sock, (struct sockaddr*) &serv_addr, sizeof(serv_addr)) < 0)
+	if(bind(listening_socket, (struct sockaddr*) &serv_addr, sizeof(serv_addr)) < 0)
 	{
 		perror("Error while binding the server port\n");
 		exit(EXIT_FAILURE);	
 	}
-
 
 	printf("Welcome, you've launched the Frogidel Torrent server.\n");
 	printf("Server is running.\n");
@@ -45,7 +44,6 @@ int main(int argc, char *argv[])
 	/*
 	* Treatment loop
 	*/
-
 	while(1)
 	{
 
