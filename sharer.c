87d090488bb4d9aedba13c64d431f03320065bb6
Videl Smith <videl@fslhome.org>
2014-05-19 22:27:14 +0200

    Started basic transmission

--- sharer.c
+++ sharer.c
@@ -12,22 +12,29 @@
 #include <strings.h>
 #include "util/metadata.h"
 #include "util/usermenu.h"
+#include "util/constants.h"
 
-void publish_new_file();
+void publish_new_file(int *serverSocket, struct sockaddr_in serv_addr);
 void search_for_a_file();
 void low_energy_server_run();
+void setup_publish_server(struct sockaddr_in *serv_addr, int *serverSocket, char *ip_addr);
 
 int main(int argc, char *argv[])
 {
-	char addr_server[30];
+	char user_addr_server[30];
 	int stop = 1;
+
+    /** Network files UDP (Central server) */
+    int serverSocket;
+    struct sockaddr_in serv_addr;
 
 	printf("Welcome! You have launched a pair!\n");
 	printf("Please write the central server's address ip: ");
 
-    read_line(addr_server, sizeof(addr_server), stdin);
+    read_line(user_addr_server, sizeof(user_addr_server), stdin);
 
-	printf("Central server's address ip: %s\n", addr_server);
+	printf("Central server's address ip: %s\n", user_addr_server);
+    setup_publish_server(&serv_addr, &serverSocket, user_addr_server);
 
 	
 	while(stop)
@@ -42,14 +49,15 @@ int main(int argc, char *argv[])
 		{
 			case 0:
 				printf("New central server's address: ");
-                read_line(addr_server, sizeof(addr_server), stdin);
+                read_line(user_addr_server, sizeof(user_addr_server), stdin);
 
-                printf("Central server's address ip: %s\n", addr_server);
+                printf("Central server's address ip: %s\n", user_addr_server);
+                setup_publish_server(&serv_addr, &serverSocket, user_addr_server);
 
 				break;
 			case 1:
 				printf("Publish a new file.\n");
-                publish_new_file();
+                publish_new_file(&serverSocket, serv_addr);
 
 				break;
 			case 2:
@@ -75,7 +83,7 @@ int main(int argc, char *argv[])
 	return EXIT_SUCCESS;
 }
 
-void publish_new_file()
+void publish_new_file(int *serverSocket, struct sockaddr_in serv_addr)
 {
     int stop = 1;
     while(stop)
@@ -83,10 +91,29 @@ void publish_new_file()
         printf("\n\n");
         printf("Please insert the file's absolute path:\n");
         char file[500];
+        int n;
+        socklen_t len = sizeof(serv_addr);
+        char sendbuf[1500];
 
         read_line(file, sizeof(file), stdin);
 
-        // TODO: PUBLISH THE FILE TO SERVER
+        // <<< PUBLISH THE FILE TO SERVER >>>
+        if ((n= sendto (*serverSocket, "PUBLISH", strlen("PUBLISH"), 0, 
+            (struct sockaddr *) &serv_addr, sizeof(serv_addr)
+            )) != strlen("PUBLISH"))
+        {
+            perror ("Sendto error");
+            exit (1);
+        }
+
+        if ( (n= recvfrom (*serverSocket, sendbuf, sizeof(sendbuf)-1,0, 
+            (struct sockaddr *) &serv_addr, &len)) < 0 )
+        {
+            printf ("erreur sendto");
+            exit (1);
+        }
+
+        printf("Received %s !\n.", sendbuf);
 
         char *usermenu_continue[1] = {"Continue"};
         int choice = print_user_menu(usermenu_continue, 1);
@@ -132,4 +159,20 @@ void search_for_a_file()
 void low_energy_server_run()
 {
 
+}
+
+void setup_publish_server(struct sockaddr_in *serv_addr, int *serverSocket, char *ip_addr)
+{
+    printf("Tried to connect to the server %s.\n", ip_addr);
+
+    memset((char *) serv_addr, 0, sizeof(&serv_addr));
+    serv_addr->sin_family = PF_INET;
+    serv_addr->sin_addr.s_addr = inet_addr(ip_addr);
+    serv_addr->sin_port = htons(PORT_ON_SERV);
+
+    if ((*serverSocket = socket(PF_INET, SOCK_DGRAM, 0)) <0)
+    {
+        perror ("Socket error.");
+        exit (1);
+    }
 }
